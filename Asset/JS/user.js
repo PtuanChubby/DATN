f7e8e27d32708378a216a2bd2f81945d94f88bec
PtuanChubby <ptuan1048@gmail.com>
2023-11-21 21:31:34 +0700

    Add new project DATN

--- Asset/JS/user.js
+++ Asset/JS/user.js
@@ -68,14 +68,13 @@ $(document).ready(function () {
   function createMemberForm(email, name, mssv, khoa) {
     var memberForm =
       '<div class="member-group">' +
-      '<div class="row">'+
+      '<div class="row">' +
       '<div class="input-group-show">' +
       '<div class="info-email">' +
       '<input type="email" class="form-control" placeholder="Email" readonly value="' +
       email +
       '" />' +
       "</div>" +
-
       '<div class="info-ht">' +
       '<input type="text" class="form-control" placeholder="Họ và tên" readonly value="' +
       name +
@@ -96,8 +95,6 @@ $(document).ready(function () {
       "</div>" +
       "</div>";
 
-      
-
     return memberForm;
   }
 
@@ -115,5 +112,75 @@ $(document).ready(function () {
     $(".close-link").click(function () {
       $(this).closest(".p_panel").remove();
     });
-    
 });
+
+function formatCurrency(number) {
+  const formattedNumber = number.toLocaleString("vi-VN", {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
+  return formattedNumber.replace(/,/g, ".");
+}
+
+function handleInputChange(event) {
+  const input = event.target;
+  const value = parseInt(input.value.replace(/\D/g, "")) || 0;
+  const formattedValue = formatCurrency(value);
+  input.value = formattedValue;
+}
+
+function calculateTotal() {
+  const inputFields = document.querySelectorAll('input[type="text"]');
+  let total_tienCong = 0;
+  let total_khoan1_1 = 0;
+  let total_khoan1_2 = 0;
+  let total_khoan1_3 = 0;
+  let total_all_dhqgHCM = 0;
+  let total_all_huyDong = 0;
+  inputFields.forEach((input) => {
+    const value = parseInt(input.value.replace(/\D/g, "")) || 0;
+    const id = input.getAttribute("id");
+    if (value > 0) {
+      if (id === "input1_dhqg" || id === "input2_1_huyDong") {
+        total_tienCong += value;
+      } else if (id === "input3_1_dhqgHCM" || id === "input3_1_huyDong") {
+        total_khoan1_1 += value;
+      } else if (id === "input3_2_dhqgHCM" || id === "input3_2_huyDong") {
+        total_khoan1_2 += value;
+      } else if (id === "input3_3_dhqgHCM" || id === "input3_3_huyDong") {
+        total_khoan1_3 += value;
+      }
+      if (id.includes("dhqg")) {
+        total_all_dhqgHCM += value;
+      } else if (id.includes("huyDong")) {
+        total_all_huyDong += value;
+      }
+    }
+  });
+  document.getElementById("total-tienCong").textContent =
+    formatCurrency(total_tienCong) + "đ";
+  document.getElementById("total-khoan1_1").textContent =
+    formatCurrency(total_khoan1_1) + "đ";
+  document.getElementById("total-khoan1_2").textContent =
+    formatCurrency(total_khoan1_2) + "đ";
+  document.getElementById("total-khoan1_3").textContent =
+    formatCurrency(total_khoan1_3) + "đ";
+  document.getElementById("total-all-dhqgHCM").textContent =
+    formatCurrency(total_all_dhqgHCM) + "đ";
+  document.getElementById("total-all-huyDong").textContent =
+    formatCurrency(total_all_huyDong) + "đ";
+  document.getElementById("total-all").textContent =
+    formatCurrency(total_all_dhqgHCM + total_all_huyDong) + "đ";
+}
+
+const inputFields = document.querySelectorAll('input[type="text"]');
+inputFields.forEach((input) => {
+  input.addEventListener("input", handleInputChange);
+  input.addEventListener("input", calculateTotal);
+});
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+)
