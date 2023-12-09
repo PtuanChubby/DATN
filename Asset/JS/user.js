e31517c23c5ebfd648d6a7963110a8492ba711f5
PtuanChubby <ptuan1048@gmail.com>
2023-12-09 18:12:23 +0700

    multi-step-form

--- Asset/JS/user.js
+++ Asset/JS/user.js
@@ -207,3 +207,75 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+// Multi Step Form
+function setupSlidePage() {
+  const slidePage = document.querySelector(".slide-page");
+  const nextBtnFirst = document.querySelector(".firstNext");
+  const prevBtnSec = document.querySelector(".prev-1");
+  const nextBtnSec = document.querySelector(".next-1");
+  const prevBtnThird = document.querySelector(".prev-2");
+  const nextBtnThird = document.querySelector(".next-2");
+  const prevBtnFourth = document.querySelector(".prev-3");
+  const submitBtn = document.querySelector(".submit");
+  const progressText = document.querySelectorAll(".step p");
+  const progressCheck = document.querySelectorAll(".step .check");
+  const bullet = document.querySelectorAll(".step .bullet");
+  let current = 1;
+
+  function nextSlide(event, percentage) {
+    event.preventDefault();
+    slidePage.style.marginLeft = percentage;
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+  }
+
+  function prevSlide(event, percentage) {
+    event.preventDefault();
+    slidePage.style.marginLeft = percentage;
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+  }
+
+  nextBtnFirst.addEventListener("click", function (event) {
+    nextSlide(event, "-25%");
+  });
+
+  nextBtnSec.addEventListener("click", function (event) {
+    nextSlide(event, "-50%");
+  });
+
+  nextBtnThird.addEventListener("click", function (event) {
+    nextSlide(event, "-75%");
+  });
+
+  submitBtn.addEventListener("click", function () {
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+    setTimeout(function () {
+      alert("Your Form Successfully Signed up");
+      location.reload();
+    }, 800);
+  });
+
+  prevBtnSec.addEventListener("click", function (event) {
+    prevSlide(event, "0%");
+  });
+
+  prevBtnThird.addEventListener("click", function (event) {
+    prevSlide(event, "-25%");
+  });
+
+  prevBtnFourth.addEventListener("click", function (event) {
+    prevSlide(event, "-50%");
+  });
+}
+
+// Call the function to setup the slide page functionality
+setupSlidePage();
+// End Multi Step Form
